72fe7f5ae285c4d9581058091ea9a2a4a9e51eb4
Nijanshu <nijanshusingh@gmail.com>
2023-09-19 19:35:13 +0530

    upadting url

--- front/src/components/Signup.js
+++ front/src/components/Signup.js
@@ -19,7 +19,7 @@ let history = useNavigate()
     e.preventDefault();
     try {
       const {name,email,password, cpassword}= creds;
-      const response = await fetch("http://localhost:5000/api/auth/createuser", {
+      const response = await fetch("https://backend-notes-mu.vercel.app/api/auth/createuser", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
